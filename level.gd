04c1505e0f28e776a0baff33b4031df8e2dd0b09
Virza Co. <aa20.virza@gmail.com>
2024-05-17 11:42:42 +0700

    sea night

---
+++ level.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var interface :OpenXRInterface = XRServer.find_interface("OpenXR")
+	if interface and interface.is_initialized():
+		get_viewport().use_xr = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
